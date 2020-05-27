ae67511bc27cb78d955684d9afd30082e0b0ee1d
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-05-27 12:55:54 -0500

    ...

--- src/reportes/fuat/fuat.controller.ts
+++ src/reportes/fuat/fuat.controller.ts
@@ -1,21 +1,33 @@
 import { Controller, Body, Post, Res, Get } from '@nestjs/common';
 
-var request =require('request');
-const client = require("jsreport-client")("http://localhost:8001")
+var request = require('request');
+var ipreportes = '172.18.20.30'
+//var ipreportes='localhost'
+const client = require("jsreport-client")("http://" + ipreportes + ":8001")
 
 @Controller('fuat')
 export class FuatController {
-    @Get('ver')
-    verFuat(@Body() Body,@Res() res){
-   
+    @Post('ver')
+    verFuat(@Body() Body, @Res() res) {
 
-         client.render({
-            template: {"shortid":"rylAk_lPoL"},options:{preview:true},"data": Body          
-           
-          }).then((response) => response.pipe(res))
-           
+        console.log(Body)
 
-      
+        /*
+                 client.render({
+                    template: {shortid:"rylAk_lPoL"},data: Body          
+                   
+                  }).then((response) => response.pipe(res))
+        */
+
+
+        client.render({
+            template: { shortid: "Hkln5SQ3iL" }, data: Body
+
+        }).then((response) => response.pipe(res))
+
+
+
+
     }
 
 }
