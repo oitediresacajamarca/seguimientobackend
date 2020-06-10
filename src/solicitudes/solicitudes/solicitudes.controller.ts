6fb7a0aaaa9b3324cbf2ef6b1cf2cf6e1dc4c3ef
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-06-10 11:03:26 -0500

    se editoi la constlta para la tabla de notificacion

--- src/solicitudes/solicitudes/solicitudes.controller.ts
+++ src/solicitudes/solicitudes/solicitudes.controller.ts
@@ -10,10 +10,10 @@ export class SolicitudesController {
         sql.close()
 
         await sql.connect('mssql://sa:.@localhost/risc_2030')
-        const consulta = `select * from SOLICITUDES_SEGUIMIENTO where cod_busqueda like '${cod_busqueda.cod_busqueda}%' ;
+        const consulta = `select * from SOLICITUDES_NOTIFICACION where cod_busqueda like '${cod_busqueda.cod_busqueda}%' ;
 `
         const result = await sql.query(consulta)
-        console.log(`select * from SOLICITUDES_SEGUIMIENTO where cod_busqueda like '${cod_busqueda.cod_busqueda}%'`)
+     
         return result;
     }
     @Get('buscarSolicitud/:cod_busqueda')
